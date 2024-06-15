01cb5f1327d6e7227b88f3046118d91b064a22b6
HEYAHONG <2229388563@qq.com>
2024-06-15 23:25:54 +0800

    完善脚本

--- FW/Core/001-base_rt/mksymlink.bat
+++ FW/Core/001-base_rt/mksymlink.bat
@@ -1,7 +1,8 @@
 @echo off
 set PROJECT_PATH=%~dp0
 echo mksymlink start!
-pushd
+pushd %PROJECT_PATH%
+echo Dir is %CD%
 if not exist HCppBox\CMakeLists.txt  mklink /D /J HCppBox  ..\3rdparty\HCppBox\master\
 popd
 echo mksymlink end!
